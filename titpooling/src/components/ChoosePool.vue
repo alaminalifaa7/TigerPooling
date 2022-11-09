eddb4abf3659a1df5b94f2e00971bf1053eb8b65
alif <alaminalifaa7@gmail.com>
2022-11-09 14:51:10 +0600

    card text from array

--- titpooling/src/components/ChoosePool.vue
+++ titpooling/src/components/ChoosePool.vue
@@ -1,3 +1,9 @@
+
+<script setup>
+const CardText = ["Offering Pool", "Requesting Pool"];
+
+</script>
+
 <template>
   <v-item-group mandatory>
     <v-container>
@@ -7,21 +13,23 @@
           :key="n"
           cols="12"
           md="6"
+     
         >
-          <v-item v-slot="{ isSelected, toggle }">
+          <v-item   v-slot="{ isSelected, toggle }">
             <v-card
               :color="isSelected ? 'primary' : ''"
-              class="d-flex align-center"
+             class=" flex-column align-center"
               dark
-              height="200"
+              height="100"
+              width="400"
               @click="toggle"
             >
               <v-scroll-y-transition>
                 <div
                   class="text-h3 flex-grow-1 text-center"
                 >
-                  {{ isSelected ? 'Offering Pool' : 'Requesting Pool' }}
-            
+                {{CardText[n-1]}}
+
                 </div>
               </v-scroll-y-transition>
             </v-card>
